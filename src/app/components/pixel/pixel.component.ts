47652cb2bb6256cf9fcd34d05f891dd250e164d1
Renzo Davila <renzo.davila@tecsup.edu.pe>
2022-11-20 17:55:00 -0500

    componetizando, mejorando sidebar y navbar

---
+++ src/app/components/pixel/pixel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pixel',
+  templateUrl: './pixel.component.html',
+  styleUrls: ['./pixel.component.sass']
+})
+export class PixelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
