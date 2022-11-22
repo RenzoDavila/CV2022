12af96edf5126ae4b514e864b73fe7089832f517
Renzo Davila <renzo.davila@tecsup.edu.pe>
2022-11-21 22:41:02 -0500

    posisionanamiento correcto, empezando scroll

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -10,6 +10,20 @@ export class ContentComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    window.addEventListener('scroll', this.scroll, true); //third parameter
   }
+
+  scroll = (event: any): void => {
+    console.log("scroll ====>", window.scrollY);
+    let position = window.scrollY
+
+    if (position != 0) {
+      window.scrollTo(0,-100000)
+    }
+    else{
+      window.scrollTo(0,100000)
+    }
+    // window.scroll(0, 0);
+  };
 
 }
